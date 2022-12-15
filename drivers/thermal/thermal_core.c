a324cbee3b89ad3318357926e19e7c6e8b86b270
Kneba <abenkenary3@gmail.com>
2022-12-15 17:43:35 +0000

    thermal: Fix NULL pointer
    Change-Id: Iaf7c85ffb30a24cef0f7b688d52800818d7504bb
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -2468,7 +2468,7 @@ void thermal_zone_device_unregister(struct thermal_zone_device *tz)
 
 	mutex_unlock(&thermal_list_lock);
 
-	thermal_zone_device_set_polling(tz, 0);
+	thermal_zone_device_set_polling(NULL, tz, 0);
 
 	if (tz->type[0])
 		device_remove_file(&tz->device, &dev_attr_type);
